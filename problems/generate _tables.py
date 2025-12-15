9403665d9840490b24085bdd4e0f092974654226
nisha <nishaa.1220@gmail.com>
2025-12-15 17:58:47 +0530

    generated tables and stored it in a file

---
+++ problems/generate _tables.py
@@ -0,0 +1,10 @@
+# create a function to generate table from 2 to 20 and store it in a file
+def generateTable(n):
+  table=""
+  for i in range(1,11):
+    table+=f"{n}*{i}={n*i}\n"
+    with open(f"tables/table_{n}.txt","w") as f:
+      f.write(table)
+
+for i in range(2,21):
+  generateTable(i)
